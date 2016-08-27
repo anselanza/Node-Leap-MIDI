daafa0940f96206b741ab7b20bb715670e7b3c11
Stephen Buchanan <stephen@mannmademedia.com>
2016-08-27 11:44:34 +0200

    sending freqShift and reverbAmount from two hands separately

--- app.js
+++ app.js
@@ -26,53 +26,43 @@ console.log("MIDI up and running...");
 
 Leap.loop(function(frame){
   if (frame.hands.length > 0) {
-    var hand = frame.hands[0];
-    var position = hand.palmPosition;
-    // process.stdout.write('palmHeight(y): ' + position[1] + '\r');
-    var palmHeight = position[1];
+    console.log(frame.hands.length);
+    // var hand = frame.hands[0];
+    // var position = hand.palmPosition;
+    // // process.stdout.write('palmHeight(y): ' + position[1] + '\r');
+    // var palmHeight = position[1];
 
-    if (palmHeight) {
-      var note = Math.round(map(palmHeight, 100, 500, 0, NUM_NOTES));
-      // process.stdout.write('note (mapped from palmHeight): ' + note + '\r');
+    var firstHandHeight = frame.hands[0].palmPosition[1];
+    console.log('firstHandHeight:', firstHandHeight);
 
+    var reverbAmount = mapClampedAndRounded(firstHandHeight, 100, 500, 0, 127);
+    console.log('reverbAmount:', reverbAmount);
+    output.sendMessage([176, 7, reverbAmount]);
 
+    if (frame.hands.length > 1) {
+      var secondHandHeight = frame.hands[1].palmPosition[1];
+      console.log('secondHandHeight:', secondHandHeight);
 
-  		// output.sendMessage([176,22,1]);
-      if (note != currentNote) {
-        if (TRAIN_INPUT == "none" || TRAIN_INPUT == "palmHeight") {
-
-          process.stdout.write('[');
-          for (var n = 0; n < NUM_NOTES; n++) {
-            if (n == note) {
-              process.stdout.write('|');
-            } else {
-              process.stdout.write('-');
-            }
-          }
-          process.stdout.write(']\n');
-
-          output.sendMessage([128,currentNote + OFFSET_PITCH,90]); // note off
-          output.sendMessage([144,note + OFFSET_PITCH,90]); // note on
-          currentNote = note;
-        }
-      }
+      var freqShift = mapClampedAndRounded(secondHandHeight, 100, 500, 0, 127);
+      console.log('freqShift:', freqShift);
+      output.sendMessage([176, 8, freqShift]);
 
     }
 
     // console.log('hand.roll:', hand.roll());
 
-    var rollPosition = 127 - Math.round(map(hand.roll(), -1, +1, 0, 127));
-
-    if (TRAIN_INPUT == "none" || TRAIN_INPUT == "rollPosition") {
-      // console.log('palm height (mapped):', rollPosition);
-      output.sendMessage([176, 7, rollPosition]);
-    }
-
-    if (TRAIN_INPUT == "none" || TRAIN_INPUT == "grabStrength") {
-      var grabStrength = 127 - Math.round(map(hand.grabStrength, 0, 1, 0, 127));
-      // console.log('grab strength (mapped):', grabStrength)
-      output.sendMessage([176, 8, grabStrength]);
-    }
+    // var rollPosition = 127 - Math.round(map(hand.roll(), -1, +1, 0, 127));
+    //
+    // if (TRAIN_INPUT == "none" || TRAIN_INPUT == "rollPosition") {
+    //   // console.log('palm height (mapped):', rollPosition);
+    //   output.sendMessage([176, 7, rollPosition]);
+    // }
+    //
+    // if (TRAIN_INPUT == "none" || TRAIN_INPUT == "grabStrength") {
+    //   var grabStrength = 127 - Math.round(map(hand.grabStrength, 0, 1, 0, 127));
+    //   // console.log('grab strength (mapped):', grabStrength)
+    //   output.sendMessage([176, 8, grabStrength]);
+    // }
 
   }
 });
@@ -92,6 +82,17 @@ function map (value, leftMin, leftMax, rightMin, rightMax) {
 
   // Convert the 0-1 range into a value in the right range.
   return rightMin + (valueScaled * rightSpan);
+}
+
+function mapClampedAndRounded(value, leftMin, leftMax, rightMin, rightMax) {
+  var mappedValue = map(value, leftMin, leftMax, rightMin, rightMax);
+  if (mappedValue > rightMax) {
+    mappedValue = rightMax;
+  }
+  if (mappedValue < rightMin) {
+    mappedValue =  rightMin;
+  }
+  return Math.round(mappedValue);
 }
 
 // Set up a new output.
